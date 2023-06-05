433c19456d20ae65052fa6886c5065d41ce0f409
Analuz Santos <108814723+AnaluzSantos@users.noreply.github.com>
2023-06-05 12:12:26 -0300

    Add files via upload

---
+++ Node/1_INTRO/2_utilizando_modulo/index.js
@@ -0,0 +1,39 @@
+//Importar os módulos 
+// fs = core module 
+// const fs = require('fs') //file system0
+// //console.log(fs)
+
+// fs.readFile('arquivo.txt', 'utf8', (err, data)=>{
+//     //console.log(err)
+//     console.log(data.length)
+// })
+
+// const fs = require('fs')
+// fs.appendFile('arquivo2.txt', 'End content', function (err){
+//     if (err) throw err;
+//     console.log('saved')
+// })
+
+// const fs = require('fs')
+// fs.open ('arquivo3.txt', 'w', function(err){
+//     if(err) throw(err)
+//     console.log('saved')
+// })
+
+// const fs = require('fs')
+// fs.writeFile('arquivo2.txt', 'Substitua todas as informações', function (err){
+//     if (err) throw (err)
+//     console.log('saved')
+// })
+
+// const fs = require('fs')
+// fs.unlink('arquivo3.txt', function (err){
+//     if(err) throw(err)
+//     console.log('File delted!')
+// })
+
+// const fs = require('fs')
+// fs.rename('arquivo2.txt', 'novoarquivo.txt', function (err){
+//     if(err) throw(err)
+//     console.log('Arquivo renomeado!')
+// })

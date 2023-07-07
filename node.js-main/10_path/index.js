1e9a61193d08d6984eeeacfd9be3a08dff2f72cc
Analuz Santos <108814723+AnaluzSantos@users.noreply.github.com>
2023-07-07 08:32:03 -0300

    Add files via upload

---
+++ node.js-main/10_path/index.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+// const custumPath = '/relatorios/analuz/relatoriosemanal.pdf'
+
+// console.log(path.dirname(custumPath))
+// console.log(path.basename(custumPath))
+// console.log(path.extname(custumPath))
+
+
+console.log(path.resolve('teste.txt'))
+
+//formatar path
+
+const midFolder = 'relatorios'
+const fileName = 'arquivo.txt'
+const finalPath = path.join('/', 'arquivos', midFolder, fileName)
+
+console.log(finalPath) 
